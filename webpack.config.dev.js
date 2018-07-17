a006f1082f91364ee15e1b5557f44093b80acc3a
David Ra Youssef <davidradeveloper@gmail.com>
2018-07-17 11:06:54 -0400

    Fix formatting

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,70 +3,70 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	devtool: 'eval',
-	entry: [
-		'./src/main.js'
-	],
-	output: {
-		path: __dirname + '/dist',
-		publicPath: '/',
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
+  devtool: 'eval',
+  entry: [
+    './src/main.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
       {
-				enforce: 'pre',
+        enforce: 'pre',
         test: /\.js$/,
-				exclude: /node_modules/,
-				use: [
+        exclude: /node_modules/,
+        use: [
           'babel-loader',
           'eslint-loader',
         ]
       },
-			{
-				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-				loader: 'file-loader?name=images/[name].[hash].[ext]'
-			},
-			{
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=images/[name].[hash].[ext]'
+      },
+      {
         test: /\.css$/,
-				use: [
-					{
-					  loader: 'style-loader'
-					},
-					{
-					  loader: 'css-loader',
-						options: {
-							modules: true,
-							importLoaders: 1,
-							localIdentName: '[name]_[local]_[hash:base64]',
-							sourceMap: true,
-							minimize: true
-						}
-					}
-				]
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.js', '.jsx']
-	},
-	devServer: {
-		contentBase: './dist',
-		hot: true,
-		historyApiFallback: true
-	},
-	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    historyApiFallback: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: __dirname + '/src/index.html',
       filename: 'index.html',
       inject: 'body'
     }),
-		new CopyWebpackPlugin([
-			{
-				from: 'src/documentation/images',
-				to: 'images'
-			}
-		])
-	]
+    new CopyWebpackPlugin([
+      {
+        from: 'src/documentation/images',
+        to: 'images'
+      }
+    ])
+  ]
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -4,66 +4,66 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: './src/main.js',
-	output: {
-		path: __dirname + '/dist',
-		filename: 'bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-				loader: 'file-loader?name=images/[name].[hash].[ext]'
-			},
-			{
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loader: 'file-loader?name=images/[name].[hash].[ext]'
+      },
+      {
         test: /\.css$/,
-				use: [
-					{
-					  loader: 'style-loader'
-					},
-					{
-					  loader: 'css-loader',
-						options: {
-							modules: true,
-							importLoaders: 1,
-							localIdentName: '[name]_[local]_[hash:base64]',
-							sourceMap: true,
-							minimize: true
-						}
-					}
-				]
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('style.css'),
-		new HtmlWebpackPlugin({
-			inject: true,
-			template: './src/index.html',
-			minify: {
-				removeComments: true,
-				collapseWhitespace: true,
-				removeRedundantAttributes: true,
-				useShortDoctype: true,
-				removeEmptyAttributes: true,
-				removeStyleLinkTypeAttributes: true,
-				keepClosingSlash: true,
-				minifyJS: true,
-				minifyCSS: true,
-				minifyURLs: true
-			}
-		}),
-		new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
-		new CopyWebpackPlugin([
-			{
-				from: 'src/documentation/images',
-				to: 'images'
-			}
-		])
-	]
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: '[name]_[local]_[hash:base64]',
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true
+      }
+    }),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' }),
+    new CopyWebpackPlugin([
+      {
+        from: 'src/documentation/images',
+        to: 'images'
+      }
+    ])
+  ]
 };
