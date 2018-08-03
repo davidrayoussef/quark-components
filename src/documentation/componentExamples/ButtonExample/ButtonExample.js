4f0eccf09313e99463803c106978b74275a17f28
David Ra Youssef <davidradeveloper@gmail.com>
2018-08-03 09:54:06 -0400

    Fix Button layout

--- src/documentation/componentExamples/ButtonExample/ButtonExample.js
+++ src/documentation/componentExamples/ButtonExample/ButtonExample.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import Button from '../../../components/Button';
-import style from './ButtonExample.css';
 
 export const ButtonExample = () => (
-  <div className={style.buttonGroup}>
+  <div>
     <Button styleType="primary">Primary</Button>
     <Button styleType="secondary">Secondary</Button>
     <Button styleType="reverse">Reverse</Button>
