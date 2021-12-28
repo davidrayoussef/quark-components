4289fe6a75ccf60e2ddc7b97670d643598e4b8e6
David Ra Youssef <davidradeveloper@gmail.com>
2021-12-28 08:06:02 -0500

    Fix Carousel tests

--- src/components/Carousel/Carousel.test.tsx
+++ src/components/Carousel/Carousel.test.tsx
@@ -1,70 +1,64 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
 import { Carousel } from './';
 
 describe('Carousel', () => {
-  const images = [
-    { title: 'image 1', src: 'images/slide-1.jpg' },
-    { title: 'image 2', src: 'images/slide-2.jpg' },
-    { title: 'image 3', src: 'images/slide-3.jpg' },
-    { title: 'image 4', src: 'images/slide-4.jpg' }
-  ];
-  const imgWidth = 500;
-  const fullImgContainerWidth = images.length * imgWidth;
-  jest.spyOn(React, 'useRef').mockReturnValue({
-    get current() {
-      return { offsetWidth: imgWidth };
-    },
-    set current(_) {}
-  });
-  beforeEach(() => {
-    render(<Carousel images={images} />);
+  const images = Array.from({ length: 4 }, (_, index) => {
+    return <img key={index} src={`images/slide-${index}.jpg`} />;
   });
   test('renders 4 images', () => {
+    render(<Carousel>{images}</Carousel>);
     expect(screen.getAllByRole('img').length).toEqual(4);
   });
+  test('renders numbers', () => {
+    render(
+      <Carousel>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+      </Carousel>
+    );
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
   test('renders arrow icons by default', () => {
-    const { container } = render(<Carousel images={images} />);
+    const { container } = render(<Carousel>{images}</Carousel>);
     expect(container.querySelectorAll('.arrowIcon').length).toEqual(2);
   });
   test('does not render arrow icons if showArrows is set to false', () => {
     const { container } = render(
-      <Carousel images={images} showArrows={false} />
+      <Carousel showArrows={false}>{images}</Carousel>
     );
     expect(container.querySelectorAll('.arrowIcon').length).toEqual(0);
   });
   test('does not render navigation dots by default', () => {
-    const { container } = render(<Carousel images={images} />);
+    const { container } = render(<Carousel>{images}</Carousel>);
     expect(container.querySelector('.dotsContainer')).not.toBeInTheDocument();
   });
   test('renders navigation dots if showDots prop is passed', () => {
-    const { container } = render(<Carousel images={images} showDots />);
+    const { container } = render(<Carousel showDots>{images}</Carousel>);
     expect(container.querySelector('.dotsContainer')).toBeInTheDocument();
   });
   test('renders Swiper by default', () => {
-    const { container } = render(<Carousel images={images} />);
+    const { container } = render(<Carousel>{images}</Carousel>);
     expect(container.querySelector('.swiperContainer')).toBeInTheDocument();
   });
   test('does not render Swiper if useSwiper prop is set to false', () => {
     const { container } = render(
-      <Carousel images={images} useSwiper={false} />
+      <Carousel useSwiper={false}>{images}</Carousel>
     );
     expect(container.querySelector('.swiperContainer')).not.toBeInTheDocument();
   });
-  test('moves image container by amount of image width on left and right arrow click', () => {
-    const { container } = render(<Carousel images={images} />);
-    const getImgContainerStyle = () =>
-      container.querySelector('.imageContainer').getAttribute('style');
-    expect(getImgContainerStyle().includes('translateX(0px)')).toBe(true);
-    fireEvent.click(container.querySelectorAll('.arrowIcon')[0]);
-    const expectedTranslateAmount = -(fullImgContainerWidth - imgWidth);
-    expect(
-      getImgContainerStyle().includes(
-        `translateX(${expectedTranslateAmount}px)`
-      )
-    ).toBe(true);
+  test('moves container 100% on left and right arrow click', () => {
+    const { container } = render(
+      <Carousel useSwiper={false}>{images}</Carousel>
+    );
+    const getContainerStyle = () =>
+      container.querySelector('.innerContainer').getAttribute('style');
+    expect(getContainerStyle().includes('translateX(-0%)')).toBe(true);
     fireEvent.click(container.querySelectorAll('.arrowIcon')[1]);
-    expect(getImgContainerStyle().includes('translateX(0px)')).toBe(true);
+    expect(getContainerStyle().includes(`translateX(-100%)`)).toBe(true);
+    fireEvent.click(container.querySelectorAll('.arrowIcon')[0]);
+    expect(getContainerStyle().includes('translateX(-0%)')).toBe(true);
   });
 });
