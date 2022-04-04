3e8e6aaf7035a0ff7094f33d6c33fa7e0a9c20a1
David Ra Youssef <davidradeveloper@gmail.com>
2022-04-04 12:15:09 -0400

    Refactor Icon test to use RTL

--- src/components/Icon/Icon.test.tsx
+++ src/components/Icon/Icon.test.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
+import { render, screen } from '@testing-library/react';
 
 import { Icon } from './';
 
 describe('Icon', () => {
-  let wrapper: ShallowWrapper;
-  beforeEach(() => {
-    wrapper = shallow(<Icon value="email" />);
-  });
   test('renders', () => {
-    expect(wrapper.exists()).toBe(true);
-  });
-  test('renders svg', () => {
-    expect(wrapper.find('svg').exists()).toBe(true);
+    const { container } = render(<Icon value="email" />);
+    screen.debug();
+    expect(container.querySelector('svg')).toBeInTheDocument();
   });
 });
